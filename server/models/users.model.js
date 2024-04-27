be82174f2e886ddc9de446e60e33a086cb9d8eeb
Akmal Solakhiddinov <104005218+akmal-solakhiddinov@users.noreply.github.com>
2024-04-27 14:37:07 +0500

    Update users.model.js

--- server/models/users.model.js
+++ server/models/users.model.js
@@ -1,9 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize('process.env.DATABASE_NAME', process.env.USERNAME, process.env.DATABASE_PASSWORD, {
-    host: process.env.DATABASE_HOST,
-    dialect: 'postgres',
-    port: 5432,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres'
 });
 
 const User = sequelize.define('User', {
