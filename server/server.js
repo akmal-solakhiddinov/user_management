d9f50f0494efe246c72b4e021c8e00a92e2a7dee
akmal-solakhiddinov1 <104005218+akmal-solakhiddinov@users.noreply.github.com>
2024-04-30 02:31:27 +0500

    update

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 4000;
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const dotenv = require('dotenv')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
-app.use(bodyParser.json())
-app.use(cors())
-dotenv.config()
+dotenv.config();
 
-const userRoute = require('./routes/userRoutes')
-app.use('/api', userRoute)
+app.use(bodyParser.json());
+app.use(cors());
+
+const userRoute = require('./routes/userRoutes');
+app.use('/api', userRoute);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
